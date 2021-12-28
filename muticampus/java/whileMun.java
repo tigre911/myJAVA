1fc0273da541ff02e49abce131a9a115530c2606
tigre911 <94420939+tigre911@users.noreply.github.com>
2021-12-28 20:42:01 +0900

    Create whileMun.java

---
+++ muticampus/java/whileMun.java
@@ -0,0 +1,18 @@
+package com.our.office;
+
+public class whileMun {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		while (true) {
+			if (i < 10) {	
+				System.out.println("잘하시는 분은 빨리 올리지 마세요 모르는 비전공자는 죽어납니다...");
+				i++;
+			}
+		}
+		
+	}
+
+}
