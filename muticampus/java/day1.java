02ca2e0d12c7e94750cf8fd68ed5314e4934ce6b
tigre911 <94420939+tigre911@users.noreply.github.com>
2021-12-28 20:41:26 +0900

    Create day1.java

---
+++ muticampus/java/day1.java
@@ -0,0 +1,45 @@
+package com.our.office;
+
+import java.awt.Window.Type;
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class day1 {
+
+	public static void main(String[] args) {
+		
+		int i;
+		
+		i = 10;
+		
+		i++;
+		
+		System.out.println(i);
+		
+		i += 1;
+		
+		System.out.println(i);
+		
+		System.out.println(i++);
+		
+		System.out.println(i);
+		
+		System.out.println(++i);
+		
+		i = +i;
+		
+		System.out.println(i);
+		
+		i = -i;
+		
+		System.out.println(i);
+		
+		int b;
+		b = -1;
+		System.out.println(b);
+		
+		
+	}
+
+}
